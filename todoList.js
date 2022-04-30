0fea2127956af4269f7e03e26443d2706f8bccfb
edsonllima <megadev@DESKTOP-GCF2849.localdomain>
2022-04-29 22:12:04 -0300

    create component FinishButton

--- todoList.js
+++ todoList.js
@@ -1,24 +1,50 @@
-const newToDo = document.querySelector("[data-form-button]");
+(() => {
+  const newToDo = document.querySelector("[data-form-button]");
+  const createTask = (event) => {
+    //imprende o refresh do formulário.
+    event.preventDefault();
+    const list = document.querySelector("[data-list]");
+    const input = document.querySelector("[data-form-input]");
+    const getValue = input.value;
 
-const createTask = (event) => {
-  //imprende o refresh do formulário.
-  event.preventDefault();
-  const list = document.querySelector("[data-list]");
-  const input = document.querySelector("[data-form-input]");
-  const getValue = input.value;
+    //cria um elemento
+    const liTask = document.createElement("li");
+    //add classe ao elento criado
+    liTask.classList.add("task");
 
-  //cria um elemento
-  const task = document.createElement("li");
-  //add classe ao elento criado
-  task.classList.add("task");
+    //Usando template strig para trabalhar html e js juntos
+    const content = `<p class="content">${getValue}</p>`;
 
-  //Usando template strig para trabalhar html e js juntos
-  const content = `<p class="content">${getValue}</p>`;
+    liTask.innerHTML = content;
 
-  task.innerHTML = content;
+    liTask.appendChild(FinishButton());
+    list.appendChild(liTask);
+    input.value = "";
+  };
 
-  list.appendChild(task);
-  input.value = "";
-};
+  newToDo.addEventListener("click", createTask);
 
-newToDo.addEventListener("click", createTask);
+  const FinishButton = () => {
+    const btnFinish = document.createElement("button");
+
+    btnFinish.classList.add("check-button");
+    //add texto no botão
+    btnFinish.innerText = "Concluir";
+    btnFinish.addEventListener("click", finishTask);
+
+    return btnFinish;
+  };
+
+  const finishTask = (event) => {
+    //pega o alvo(elemento) que sofreu a ação
+    const btnFinish = event.target;
+    //basca elemento pai
+    const completeTask = btnFinish.parentElement;
+    //toggle trabalha com boleano true and false no click
+    completeTask.classList.toggle("done");
+  };
+
+  // const DeleteButton = () => {
+  //   const deleteButton = document.createElement("button");
+  // };
+})();
